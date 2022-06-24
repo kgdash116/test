0f71a21755a055db58d87c1089453227f3f3aab7
kgdash116 <101527504+kgdash116@users.noreply.github.com>
2022-06-24 05:48:33 +0500

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,11 +1,5 @@
 hungry=input("are you hungry? ")
 if hungry=="yes":
-    print("ok cool")
-    print("ok cool go eat pizza")
-    print("and then take a nap")
-    print("eat fries")
+    print("eat samosa")
 else :
-    thirsty=input("are you thirsty")
-    if thirsty=="yes":
-        print("heres some water")
-        print("drink soda")
+    print("do homework")
